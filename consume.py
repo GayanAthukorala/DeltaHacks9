37b667ab86c3ae0fdba4a45d198d3ad17a604fb6
Ahren Chen <ahren@hotmail.ca>
2023-01-15 02:07:56 -0500

    Created a function to remove all small businesses from the current database for reuse with new locations.

--- consume.py
+++ consume.py
@@ -15,4 +15,8 @@ def RemoveBigCompanies(companies):
     return companies
 
 def DeleteAllSmallBusiness():
-    url = 'http://127.0.0.1:8000/DeltaHacks9Small/'
+    url = 'http://127.0.0.1:8000/DeltaHacks9Small'
+    response = requests.get(url)
+    list_of_companies = response.json()
+    for company in list_of_companies:
+        requests.delete(url + '/' + str(company['id']))
