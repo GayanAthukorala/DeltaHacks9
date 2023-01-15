3f212cc79c449850dbe1a49d507573a6d8882a23
Ahren Chen <ahren@hotmail.ca>
2023-01-15 01:05:48 -0500

    Created python function to erase list of big companies from a 2d list of all businesses in the list.
    Assuming the input is a 2D list containing the name of the businesses and the URL linking to a google map of that business location, this function will return a list of similar format, but without any of the big businesses. Future modifications will include inputting the small businesses into the small business database.

--- consume.py
+++ consume.py
@@ -1,4 +1,10 @@
 import requests
 
-response = requests.get('http://127.0.0.1:8000/DeltaHacks9Big')
-print(response.json())
+def RemoveBigCompanies(companies):
+    companies = [['yes', 2], ['A&W', 3]]
+    response = requests.get('http://127.0.0.1:8000/DeltaHacks9Big')
+    list_of_big_business = response.json()
+    for business in list_of_big_business:
+        companies = [i for i in companies if i[0] != business['name']]
+    print(companies)
+
